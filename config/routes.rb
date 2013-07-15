9d46ce2e9687ca9ade91360d8f7b415b29fce254
Gabe <gabedominguez@outlook.com>
2013-07-14 20:21:37 -0700

    added attr_accesible to admin_user.rb and rake db:migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Admintest::Application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
   root :to => 'welcome#index'
 
